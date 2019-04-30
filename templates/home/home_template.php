53f7c3bb3a6ac6320d1871d253d0600398c6bbec
jcarlos <jcpablo10@gmail.com>
2019-04-30 12:40:47 -0500

    arreglos bullets verdes

--- templates/home/home_template.php
+++ templates/home/home_template.php
@@ -10,35 +10,44 @@
 </header>
 <section class="container internacional">
     <div class="grid-container international-grid-container">
-        <h2>Panorama Internacional</h2>
+        <h2 class="title-2">Panorama Internacional</h2>
         <h3>Enfermedades infectocontagiosas prevenibles por vacunación</h3>
-        <p>  La Organización Mundial de la Salud, señala que las 
-            principales causas de muerte infantil a nivel mundial 
-            son las neumonías u otras enfermedades respiratorias 
-            agudas y las enfermedades diarreicas ambas prevenibles 
-            mediante la vacunación.
-        </p>
-        <p>
-            Gracias a los avances en materia de vacunación, se 
-            puede erradicar la poliomielitis a nivel mundial en 
-            los próximos años.
-        </p>
-        <p>
-            México tiene el compromiso a nivel mundial de aumentar 
-            sus tasas de vacunación por arriba del 95%. Solo
-            uniendo esfuerzos podemos alcanzar los objetivos 
-            nacionales y mejorar la salud de los mexicanos.
-        </p>
+        <div class="box-container">
+            <p>  La Organización Mundial de la Salud, señala que las 
+                principales causas de muerte infantil a nivel mundial 
+                son las neumonías u otras enfermedades respiratorias 
+                agudas y las enfermedades diarreicas ambas prevenibles 
+                mediante la vacunación.
+            </p>
+            <p>
+                Gracias a los avances en materia de vacunación, se 
+                puede erradicar la poliomielitis a nivel mundial en 
+                los próximos años.
+            </p>
+            <p>
+                México tiene el compromiso a nivel mundial de aumentar 
+                sus tasas de vacunación por arriba del 95%. Solo
+                uniendo esfuerzos podemos alcanzar los objetivos 
+                nacionales y mejorar la salud de los mexicanos.
+            </p>
+        </div>
+        
 
     </div>
 </section>
 <section class="container vacunas-cifras">
     <div class="grid-container vacunas-cifras-grid">
-        <ul>
+    <h2 class="title-2">Las vacunas en cifras</h2>
+        <ul class="primary-list">
             <li>
                 <p>
                     Cada año se previenen entre 2.5 a 3 millones 
                     de muertes gracias a las vacunas.  
+                    
+                </p>
+            </li>
+            <li>
+                <p>
                     La Organización Mundial de la Salud destaca que 
                     se podrían salvar 1.5 millones de vidas más 
                     si se mejora la cobertura de vacunación en todos los países.
