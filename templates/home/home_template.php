8f6e2c81670f9226072fbdc4c5152a7da41b5e79
jcarlos <jcpablo10@gmail.com>
2019-06-18 15:08:41 -0500

    quines somos

--- templates/home/home_template.php
+++ templates/home/home_template.php
@@ -8,63 +8,106 @@
         <i>Twitter</i>
     </div>
 </header>
-<section class="container internacional">
+<section class="container quienes-somos">
     <div class="grid-container international-grid-container">
-        <h2 class="title-2">Panorama Internacional</h2>
-        <h3>Enfermedades infectocontagiosas prevenibles por vacunación</h3>
+        <h2 class="title-2">¿Quiénes somos?</h2>
+        
         <div class="box-container">
-            <p>  La Organización Mundial de la Salud, señala que las 
-                principales causas de muerte infantil a nivel mundial 
-                son las neumonías u otras enfermedades respiratorias 
-                agudas y las enfermedades diarreicas ambas prevenibles 
-                mediante la vacunación.
-            </p>
             <p>
-                Gracias a los avances en materia de vacunación, se 
-                puede erradicar la poliomielitis a nivel mundial en 
-                los próximos años.
-            </p>
-            <p>
-                México tiene el compromiso a nivel mundial de aumentar 
-                sus tasas de vacunación por arriba del 95%. Solo
-                uniendo esfuerzos podemos alcanzar los objetivos 
-                nacionales y mejorar la salud de los mexicanos.
+                Somos una iniciativa ciudadana preocupada por la salud de 
+                los mexicanos, que busca promover la cultura de la 
+                prevención a través de la vacunación y del fomento de 
+                hábitos higiénicos. Apoyada por un equipo 
+                multidisciplinario con la finalidad de disminuir las 
+                enfermedades infectocontagiosas.
             </p>
         </div>
-        
+    </div>
+</section>
+<section class="container">
+    <div class="grid-container">
+        <h3>Logros</h3>
+        <p>
+            Generación sinergias con diversos actores de salud 
+            pública para promover en conjunto la vacunación 
+            como mejor herramienta costo-efectiva de salud para 
+            el control y la eliminación de enfermedades prevenibles 
+            por vacunación.
+        </p>
+        <p>
+            Publicación de contenidos informativos en materia de prevención de enfermedades.
+        </p>
+    </div>
+</section>
+<section class="container">
+    <div class="grid-container">
+        <h2>Nuestras acciones</h2>
+            <ul>
+                <li>
+                    <p>  Generar campañas de promoción a la salud y a la vacunación de manera específica.</li></p>
+                    <img src="" alt="">
+                <li>
+                    <p>  Publicación de materiales educativos en el tema de vacunación.</li></p>
+                    <img src="" alt="">
+                <li>
+                    <p>  Difusión de información básica de a través de redes sociales.</li></p>
+                    <img src="" alt="">
+                <li>
+                    <p>  Organización de Foros y mesas de debate.</li></p>
+                    <img src="" alt="">
+                <li>
+                    <p>  Sesiones y talleres educativos.</li></p>
+                    <img src="" alt="">
+            </ul>
+            
 
     </div>
 </section>
-<section class="container vacunas-cifras">
-    <div class="grid-container vacunas-cifras-grid">
-    <h2 class="title-2">Las vacunas en cifras</h2>
-        <ul class="primary-list">
-            <li>
-                <p>
-                    Cada año se previenen entre 2.5 a 3 millones 
-                    de muertes gracias a las vacunas.  
+<section class="container">
+    <section class="grid-container">
+        <h2>Nuestro equipo</h2>
+
+        <div class="box-container">
+            <img src="" alt="">
+            <h3>Blanca E. Rodriguez Jaramillo</h3>
+            <span class="title-photo">Directora General</span>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Consequatur nulla commodi minima minus deserunt vero quasi 
+                aperiam aliquam corporis. Nesciunt ad animi ducimus possimus 
+                odio eius exercitationem. Optio, recusandae tempora.
+            </p>
+        </div>
+    </section>
+</section>
+<section class="container">
+    <section class="grid-container">
+        <h2>Contacto</h2>
+        <a href="#mailto">info@curarseensalud.org</a>
+        <i>Facebook</i>
+        <i>Twitter</i>
+        <div class="form box-container">
+            <form action="">
+                <h3>Ponte en contacto</h3>
+                <div class="input-container">
+                    <label for=""></label>
+                    <input type="text" name="" id="">
+                </div>
+                <div class="input-container">
+                    <label for=""></label>
+                    <input type="text" name="" id="">
+                </div>
+                <div class="input-container">
+                    <textarea name="textarea" rows="10" cols="50">Write something here</textarea>
+                </div>
+                <div class="input-container">
                     
-                </p>
-            </li>
-            <li>
-                <p>
-                    La Organización Mundial de la Salud destaca que 
-                    se podrían salvar 1.5 millones de vidas más 
-                    si se mejora la cobertura de vacunación en todos los países.
-                </p>
-            </li>
-            <li>
-                <p>La tasa de vacunación en la población infantil se ha estancado en el 86%.</p>
-            </li>
-            <li>
-                <p>En el mundo aún hay 19.5 millones de lactantes sin vacunar.</p>
-            </li>
-            <li>
-                <p>Por cada dólar que se invierte en vacunas, se ahorran 44.</p>
-            </li>
-            <li>
-                <p>Para mejorar la calidad de vida de las personas se debe mantener una cobertura de vacunación por arriba del 95%.</p>
-            </li>
-        </ul>
-    </div>
+                    <input type="checkbox" id="cbox2" value="second_checkbox"> <label for="cbox2">Este es mi segundo checkbox</label>
+                </div>
+                <div class="input-container">
+                    <button>Enviar</button>
+                </div>
+            </form>
+        </div>
+    </section>
 </section>
