dbb9792433a63bfd926236c81c6cd880ddaa5fab
jcarlos <jcpablo10@gmail.com>
2019-06-24 11:07:07 -0500

    WIP nos mueve

--- templates/mueve/mueve.php
+++ templates/mueve/mueve.php
@@ -17,3 +17,69 @@
         </div>
     </div>
 </section>
+<section class="container logros">
+    <div class="grid-container">
+        <h3>Logros</h3>
+        <p>
+            Generación sinergias con diversos actores de salud 
+            pública para promover en conjunto la vacunación 
+            como mejor herramienta costo-efectiva de salud para 
+            el control y la eliminación de enfermedades prevenibles 
+            por vacunación.
+        </p>
+        <p>
+            Publicación de contenidos informativos en materia de prevención de enfermedades.
+        </p>
+    </div>
+</section>
+<section class="container">
+    <div class="grid-container">
+        <h3>Educar y concientizar</h3>
+        <div class="educar-concientizar">
+            <ul>
+            <li>    Generar campañas de promoción a la salud y a la vacunación de manera específica.</li>
+            <li>    Publicación de materiales educativos en el tema de vacunación.</li>
+            <li>    Difusión de información básica de a través de redes sociales.</li>
+            <li>    Organización de Foros y mesas de debate.</li>
+            <li>    Sesiones y talleres educativos.</li>
+
+            </ul>
+        </div>
+    </div>
+    <div class="grid-container">
+        <h4>Promocionar medidas adicionales para evitar las enfermedades</h4>
+        <ol>
+            <li>
+                <img src="assets/images/acciones/generar.svg" alt="">
+                <p>Campañas de hábitos higiénicos.</p>
+            </li>
+            <li>
+                <img src="assets/images/nos-mueve/alimentacion.svg" alt="">
+                <p>Alimentación correcta.</p>
+            </li>
+            <li>
+                <img src="assets/images/nos-mueve/antibioticos.svg" alt="">
+                <p>Uso adecuado de antibióticos.</p>
+            </li>
+        </ol>
+    </div>
+</section>
+<section class="container tarea">
+    <div class="grid-container">
+        <h3>Nuestra Tarea</h3>
+        <div class="box-container w100">
+            <h6>Seguimiento y monitoreo</h6>
+            <ul>
+                  <li>Cumplimiento de normas básicas de salud.</li>
+                  <li>Evaluación del cumplimiento de las estrategias de salud.</li>
+                  <li>Mantenimiento de las normas preventivas de salud.</li>
+            </ul>
+            <h6>Impulsar políticas públicas</h6>
+                <ul>
+                    <li>Generar propuestas de prevención.</li>
+                    <li>Nuevas propuestas de cobertura en medidas preventivas existentes.</li>
+
+                </ul>
+        </div>
+    </div>
+</section>
