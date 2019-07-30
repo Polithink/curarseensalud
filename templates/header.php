d0b1f4aa491fb17f0ddff9723481fb014f8c657c
jcarlos <jcpablo10@gmail.com>
2019-07-30 13:47:32 -0500

    gallery

--- templates/header.php
+++ templates/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Curarse en Salud</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/gallery/blueimp-gallery.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/ces.css">
 </head>
