d41e4fdd2352388c70a2fb026a2e187a04ae2f33
jcarlos <jcpablo10@gmail.com>
2020-04-08 18:53:48 -0500

    header nav

--- templates/nav.php
+++ templates/nav.php
@@ -5,7 +5,38 @@
         <div class="bottom-line"></div>
     </div>
 </div>
+<div class="header__nav">
+    <div class="header__nav-container">
+        <div class="header__nav-logo">
+            <a href="./inicio">
+                <img src="assets/images/ces-logo-bco.png" alt="" class="header__logo-img">
+            </a>
+        </div>
+        <div class="header__nav-menu">
+            <ul class="header__menu-list">
+                <!-- <li>
+                    <a href="./inicio">Inicio</a>
+                </li> -->
+                <li>
+                    <a href="./quienes">¿Quienes somos?</a>
+                </li>
+                <li>
+                    <a href="./nosmueve">Lo que nos mueve</a>
+                </li>
+                <li>
+                    <a href="./panorama">Panorama internacional</a>
+                </li>
+                <li>
+                    <a href="./enfermedades-por-vector">Enfermedades por vector</a>
+                </li>
+                <li>
+                    <a href="./descargas">Descargas</a>
+                </li>
+            </ul>
+        </div>
 
+    </div>
+</div>
 <nav class="">
     <!-- <img src="assets/images/ces-logo-bco.png" alt="" class="logo-menu"> -->
     <ul>
